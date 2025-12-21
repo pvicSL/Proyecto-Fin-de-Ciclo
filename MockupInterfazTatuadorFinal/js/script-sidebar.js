a346b398948e8b88f0eba5aa537867a5348ec41d
Santiago <santimuner9@gmail.com>
2025-12-21 16:11:39 +0100

    UPDATE: añadimos responsive y comentarios

--- MockupInterfazTatuadorFinal/js/script-sidebar.js
+++ MockupInterfazTatuadorFinal/js/script-sidebar.js
@@ -2,8 +2,10 @@
 -- SCRIPT PARA EL SIDEBAR --  
 ============================ */
 
+// Obtengo los elementos del HTML
 const hamburger = document.querySelector('#toggle-btn');
 
+// Función que al darle click expande el sidebar
 hamburger.addEventListener('click', function(){
     document.querySelector('#sidebar').classList.toggle('expand')
 })
