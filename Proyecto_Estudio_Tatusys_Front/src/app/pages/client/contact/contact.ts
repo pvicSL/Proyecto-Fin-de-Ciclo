dffb3b8c3d37d895946ad9df11dacad2ffa11981
Patricia SL <patriciavictori.sanz417@comunidadunir.net>
2026-02-28 13:26:29 +0100

    UPDATE: eliminada visualización datos factura en form. fixes varios

--- Proyecto_Estudio_Tatusys_Front/src/app/pages/client/contact/contact.ts
+++ Proyecto_Estudio_Tatusys_Front/src/app/pages/client/contact/contact.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppointmentService } from '../../../core/services/appointment.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-contact',
     standalone: true,
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, RouterLink],
     templateUrl: './contact.html',
     styleUrl: './contact.css'
 })
